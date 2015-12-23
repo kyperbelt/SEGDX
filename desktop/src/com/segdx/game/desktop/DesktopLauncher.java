751671a52ab409612399c8b3a25dc820848963a2
kyper <jonathancamarenacamcho@gmail.com>
2015-12-23 11:49:35 -0800

    clean project
    libgdxjam entry clean project

---
+++ desktop/src/com/segdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.segdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.segdx.game.SEGDX;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SEGDX(), config);
+	}
+}
